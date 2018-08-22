3e8933e602f85312347de597ca9f239de0234588
gzxultra <mygladfinger@gmail.com>
2018-08-22 14:59:34 -0400

    Fix wrong reference of graph variable

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -2,7 +2,7 @@
 from src.dijkstra import dijkstra
 
 
-def test_dijkstra(graph):
+def test_dijkstra(graph1):
     src, dst = 0, 2
-    result = dijkstra(graph, src, dst)
+    result = dijkstra(graph1, src, dst)
     assert result == ({0: 0, 1: 100, 2: 300, 3: 200}, {0: None, 1: None, 2: 3, 3: 1})
