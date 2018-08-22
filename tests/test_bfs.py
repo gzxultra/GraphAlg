652b311904119e3394e331dd6c3b8fffe58f935b
gzxultra <mygladfinger@gmail.com>
2018-08-22 10:26:13 -0400

    Fix wrong inference

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -3,9 +3,9 @@ from src.bfs import bfs_iterative, bfs_recursive
 
 def test_bfs_iterative(graph2):
     src, dst = 1, 6
-    assert bfs_iterative(graph, src, dst) == [1, 2, 3, 4, 5, 6]
+    assert bfs_iterative(graph2, src, dst) == [1, 2, 3, 4, 5, 6]
 
 
-def test_bfs_iterative(graph2):
+def test_bfs_recursive(graph2):
     src, dst = 1, 6
-    assert bfs_iterative(graph, src, dst) == [1, 2, 3, 5, 3, 6]
+    assert bfs_recursive(graph2, src, dst) == [1, 2, 3, 4, 5, 6]
