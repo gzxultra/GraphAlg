4ff0c6198058f883b4fc181c02fbcca72a7d586d
gzxultra <mygladfinger@gmail.com>
2018-08-22 16:25:59 -0400

    Implement basic version of Floyd-Marshall

---
+++ src/floyd_warshall.py
@@ -0,0 +1,18 @@
+def floyd_warshall(G):
+    """ Floyd–Warshall algorithm
+        pseudocode by wikipedia: https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+    """
+    n_nodes = len(G.nodes)
+    dist = [[float('inf') for i in range(0, n_nodes)] for j in range(0, n_nodes)]
+
+    for from_node in G.nodes:
+        for to_node in G.edges[from_node]:
+            dist[from_node][to_node] = G.distances[(from_node, to_node)]
+
+    for k in G.nodes:
+        for i in G.nodes:
+            for j in G.nodes:
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    return dist
