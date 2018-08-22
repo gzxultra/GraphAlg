720707b6a29acd84fef6d8a40a25ec211f5572aa
gzxultra <mygladfinger@gmail.com>
2018-08-22 10:20:39 -0400

    Restructure test code

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -1,13 +1,11 @@
-def test_bfs_iterative():
-    from src.libs import Graph
-    from src.bfs import bfs_iterative
-    edges = [[1,2], [1,3], [2,4], [2,5], [3,6]]
-    nodes = list(range(1, 6))
-    src, dst = 1, 6
-    graph = Graph()
-    for node in nodes:
-        graph.add_node(node)
-    for (from_node, to_node) in edges:
-        graph.add_edge(from_node, to_node)
+from src.bfs import bfs_iterative, bfs_recursive
 
+
+def test_bfs_iterative(graph2):
+    src, dst = 1, 6
     assert bfs_iterative(graph, src, dst) == [1, 2, 3, 4, 5, 6]
+
+
+def test_bfs_iterative(graph2):
+    src, dst = 1, 6
+    assert bfs_iterative(graph, src, dst) == [1, 2, 3, 5, 3, 6]
--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,17 +1,8 @@
 
+from src.dijkstra import dijkstra
 
-def test_dijkstra():
-    from src.dijkstra import dijkstra
-    from src.libs import Graph
 
-    n_cities = 4
-    flights = [[0,1,100],[0,3,300],[0,2,500],[1,3,100],[3,2,100]]
+def test_dijkstra(graph):
     src, dst = 0, 2
-
-    graph = Graph()
-    for node in range(0, n_cities):
-        graph.add_node(node)
-    for (_src, _dist, _cost) in flights:
-        graph.add_edge(_src, _dist, _cost)
     result = dijkstra(graph, src, dst)
     assert result == ({0: 0, 1: 100, 2: 300, 3: 200}, {0: None, 1: None, 2: 3, 3: 1})
