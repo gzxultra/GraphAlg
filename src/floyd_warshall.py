1085bc52c001b8f0e26182a54af3cb4f0b6053fa
gzxultra <mygladfinger@gmail.com>
2018-08-23 00:18:47 -0400

    Add Floyd-Warshall algorithm with path reconstruction

--- src/floyd_warshall.py
+++ src/floyd_warshall.py
@@ -15,3 +15,36 @@ def floyd_warshall(G):
                 if dist[i][j] > dist[i][k] + dist[k][j]:
                     dist[i][j] = dist[i][k] + dist[k][j]
     return dist
+
+
+def floyd_warshall_with_path_reconstruction(G):
+    """ Floyd–Warshall algorithm
+        pseudocode by wikipedia: https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+    """
+    n_nodes = len(G.nodes)
+    dist = [[float('inf') for i in range(0, n_nodes)] for j in range(0, n_nodes)]
+    next_move = [[float('inf') for i in range(0, n_nodes)] for j in range(0, n_nodes)]
+
+    for from_node in G.nodes:
+        for to_node in G.edges[from_node]:
+            dist[from_node][to_node] = G.distances[(from_node, to_node)]
+            next_move[from_node][to_node] = to_node
+
+    for k in G.nodes:
+        for i in G.nodes:
+            for j in G.nodes:
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    next_move[i][j] = next_move[i][k]
+    return dist, next_move
+
+
+def reconstruct_path(next_move, u, v):
+    if not next_move[u][v]:
+        return []
+
+    path = [u]
+    while u != v:
+        u = next_move[u][v]
+        path.append(u)
+    return path
--- tests/test_floyd_warshall.py
+++ tests/test_floyd_warshall.py
@@ -1,4 +1,5 @@
-from src.floyd_warshall import floyd_warshall
+from src.floyd_warshall import (floyd_warshall,
+    floyd_warshall_with_path_reconstruction, reconstruct_path)
 
 
 def test_floyd_warshall(graph1):
@@ -7,3 +8,13 @@ def test_floyd_warshall(graph1):
     assert dist[0][2] == 300
     assert dist[0][3] == 200
     assert dist[1][2] == 200
+
+
+def test_floyd_warshall_with_path_reconstruction(graph1):
+    dist, next_move = floyd_warshall_with_path_reconstruction(graph1)
+
+    path = reconstruct_path(next_move, 0, 3)
+    assert path == [0, 1, 3]
+
+    path = reconstruct_path(next_move, 0, 2)
+    assert path == [0, 1, 3, 2]
