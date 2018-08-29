9ca203a5451636f2c32ff47494c60c2f57f94ddd
gzxultra <mygladfinger@gmail.com>
2018-08-28 23:22:17 -0400

    Fix ci test

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -5,4 +5,4 @@ from src.dijkstra import dijkstra
 def test_dijkstra(graph1):
     src, dst = 0, 2
     result = dijkstra(graph1, src, dst)
-    assert result == ({0: 0, 1: 100, 2: 300, 3: 200}, {0: None, 1: None, 2: 3, 3: 1})
+    assert result == ({0: 0, 1: 100, 2: 300, 3: 200}, {0: None, 1: 0, 2: 3, 3: 1})
